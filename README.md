3453faf3e405913ae6d0cd0f4ee59f25387ea9e2
harikrishna76 <harikrishna.salver@gmail.com>
2021-07-31 20:42:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-v5lejg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-v5lejg)
---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(app.get('port'), function() {
+  console.log(
+    'http Express server(worker) listening on port ' + app.get('port')
+  );
+});
